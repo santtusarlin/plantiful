35f517589f95a0085578e15693205f9ae5423ab8
Mikko Hannukainen <mikkohannukainen@hotmail.com>
2019-02-21 12:08:28 +0200

    Kasvin demottamisen techi

--- plantiful-app/src/app/plant/plant.component.ts
+++ plantiful-app/src/app/plant/plant.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page';
+import { Image } from "tns-core-modules/ui/image";
+
 @Component({
   selector: 'ns-plant',
   templateUrl: './plant.component.html',
@@ -8,10 +10,27 @@ import { Page } from 'tns-core-modules/ui/page';
 })
 export class PlantComponent implements OnInit {
 
+
+  kakkaimg1: Image;
+  kakkaimg2: Image;
+  isShown: boolean = true;
   constructor(private page: Page) { }
 
   ngOnInit() {
     this.page.actionBarHidden = true;
+    const imaage = new Image();
+    imaage.src = "res://plant1";
+    const imaage2 = new Image();
+    imaage2.src = "res://plant2";
+    this.kakkaimg1 = imaage;
+    this.kakkaimg2 = imaage2;
   }
 
+  toggleShown() {
+    if (this.isShown) {
+      this.isShown = false;
+    } else {
+      this.isShown = true;
+    }
+  }
 }
