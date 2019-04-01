62ae80e0b1ce24bf36f5bab075b0a1a6b7259e69
Santtu Sarlin <k8644@student.jamk.fi>
2019-04-01 09:09:19 +0300

    kaaviota, ei toimi :(

--- plantiful-app/src/app/stats/graph/graph.component.ts
+++ plantiful-app/src/app/stats/graph/graph.component.ts
@@ -14,22 +14,78 @@ const firebase = require("nativescript-plugin-firebase/app");
 })
 export class GraphComponent implements OnInit {
 
-  public entries: Array<any> = [];
-
+  //public entriesx: Array<any> = [{ date: new Date, mood: 1}, { date: new Date, mood: 3}];
+  private _entries: ObservableArray<any>
     constructor(private page: Page) { }
 
-    ngOnInit() {
+    get entries(): ObservableArray<any> {
+      return this._entries;
+    }
 
+    getEntries() {
+      // let entries = [];
+      // const collection = firebase.firestore().collection("users").orderBy("date", "desc");
+
+      // collection.get().then(querySnapshot => {
+      // querySnapshot.forEach(doc => {
+      //   entries.push(doc.data())
+      //   });
+      // console.log(entries);
+      // return entries;
+      // }); 
+      const date1 = new Date('January 23, 2019 15:24:00');
+      const date2 = new Date('January 25, 2019 15:24:00');
+      const date3 = new Date('February 23, 2019 15:24:00');
+      let mockdata = [
+              { 
+              date: date1, mood: 1, 
+              activities: [
+                { imageURL: "beer.png",
+                  title: "beer",
+                  selected: "true"
+                }
+              ],
+              freeText: "lol xd1",
+              imageURL: "Flower1.png"
+              },
+              { 
+              date: date2, mood: 2, activities: [
+                { imageURL: "beer.png",
+                  title: "beer",
+                  selected: "true"
+                }
+              ],
+              freeText: "lol xd2",
+              imageURL: "Flower2.png"
+              },
+              { 
+              date: date3, mood: 3, activities: [
+                { imageURL: "hiking.png",
+                  title: "hiking",
+                  selected: "true"
+                }
+              ],
+              freeText: "lol xd3",
+              imageURL: "Flower3.png"
+              }
+            ];
+            console.log(mockdata);
+            return mockdata;
+    }
+
+    ngOnInit() {
+      this._entries = new ObservableArray(this.getEntries());
+    
         // this._dateTimeSource = new ObservableArray(this._dataService.getDateTimeSource());
         // console.log(this.dateTimeSource);
         
-        const collection = firebase.firestore().collection("users").orderBy("date", "desc");
+        // const collection = firebase.firestore().collection("users").orderBy("date", "desc");
 
-        collection.get().then(querySnapshot => {
-          querySnapshot.forEach(doc => {
-            this.entries.push(doc.data())
-          });
-        }); 
-        console.log(this.entries);
+        // collection.get().then(querySnapshot => {
+        //   querySnapshot.forEach(doc => {
+        //     this.entries.push(doc.data())
+        //   });
+        //   console.log(this.entries);
+        // }); 
     }
 }
