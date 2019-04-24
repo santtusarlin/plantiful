b54aade629194fde8a9fa49ac059acd42121b859
Mikko Hannukainen <mikkohannukainen@hotmail.com>
2019-04-24 14:40:20 +0300

    bäkkärigroundi

--- plantiful-app/src/app/plant/plant.component.ts
+++ plantiful-app/src/app/plant/plant.component.ts
@@ -21,7 +21,7 @@ export class PlantComponent implements OnInit {
 
   ngOnInit() {
     this.page.actionBarHidden = true;
-    this.page.backgroundImage = "~/app/images/background/sky1.png";
+    this.page.backgroundImage = "~/app/images/background/sky3.png";
 
     const collection = firebase.firestore().collection(`${this.uuid.uuid}`).orderBy("date", "desc");
     let data = [];
